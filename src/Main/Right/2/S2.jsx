7e07a56bafd79db427b0d2928b0985b9aa1f839e
AkhmetoSR <ahmetov26@gmail.com>
2024-10-28 17:18:34 +0500

    //job

--- src/Main/Right/2/S2.jsx
+++ src/Main/Right/2/S2.jsx
@@ -3,8 +3,35 @@ import { motion} from "framer-motion";
 
 function S2() {
     return (
-        <motion.div initial={{ opacity: 0, scale: 0.9 }} animate={{ opacity: 1, scale: 1 }} transition={{duration: 0.2}} className={s.S2}>
-
+        <motion.div initial={{ opacity: 0 }} animate={{ opacity: 1 }} transition={{duration: 0.4}} className={s.S2}>
+            <div className={s.Structure}>
+                <h3>Структура и органы управления образовательной организацией</h3>
+            </div>
+            <div>
+                <p>Генеральный директор АО «РН-Няганьнефтегаз»</p>
+                <p>Муслимов Эдуард Ядкарович</p>
+            </div>
+            <div>
+                <p>Заместитель генерального директора по персоналу и социальным программам</p>
+                <p>Неугомонова Алина Петровна</p>
+            </div>
+            <div>
+                <p>Начальник Учебного центра</p>
+                <p>Миронова Анастасия Ивановна</p>
+            </div>
+            <div>
+                <p>Юридический адрес: Ханты-Мансийский автономный округ –Югра, г. Нягань, ул. Сибирская, дом 10, корпус 1</p>
+                <p>АО «РН-Няганьнефтегаз» телефон: 8 (34672) 92-222</p>
+                <p>факс: 8 (34672)94-444</p>
+                <p>e-mail: rn_nng@nng.rosneft.ru</p>
+            </div>
+            <div>
+                <p>Начальник Учебного центра АО «РН-Няганьнефтегаз»:</p>
+                <p>Миронова Анастасия Ивановна</p>
+                <p>Телефон: 8 (34672) 92-222 доб 1536</p>
+                <p>E-mail: aimironova@nng.rosneft.ru</p>
+            </div>
+            <div></div>
         </motion.div>
     );
 }
