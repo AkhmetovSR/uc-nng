71db5db726225623d55cf655f76857b63666f44a
AkhmetoSR <ahmetov26@gmail.com>
2024-10-29 16:20:04 +0500

    //job

--- src/Main/Right/Right.jsx
+++ src/Main/Right/Right.jsx
@@ -2,7 +2,7 @@ import s from "./Right.module.css";
 import S1 from "./1/S1.jsx";
 import S2 from "./2/S2.jsx";
 import S3 from "./3/S3.jsx";
-import {BrowserRouter, Navigate, Route, Routes} from "react-router-dom";
+import {Route, Routes} from "react-router-dom";
 
 function Right() {
     return (
